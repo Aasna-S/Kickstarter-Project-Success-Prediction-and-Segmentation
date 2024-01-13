3b49879ea15fdd42ce345b899c639eedd0ee71d3
Aasna-S <142115500+Aasna-S@users.noreply.github.com>
2024-01-13 14:07:48 -0500

    Update Kickstarter.py

--- Kickstarter.py
+++ Kickstarter.py
@@ -17,7 +17,7 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from sklearn.metrics import silhouette_score
 
-kickstarter_data = pd.read_excel("Kickstarter-Project-Success-Prediction-and-Segmentation/Kickstarter.xlsx")
+kickstarter_data = pd.read_excel("Kickstarter.xlsx")
 
 ####Random Forest#########
 
